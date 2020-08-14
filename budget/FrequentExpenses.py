9bc315f26bb55d459db831afdfef50effa63b0a2
Julian Cuentas <juliancuentas@hotmail.com>
2020-08-14 20:48:24 -0300

    second module

---
+++ budget/FrequentExpenses.py
@@ -0,0 +1,36 @@
+# First local modules
+from . import Expense
+
+# Second, built-in modules
+import collections
+
+# Third party modules
+import matplotlib.pyplot as plt
+
+# With this I construct the class
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+spending_categories = []
+
+
+for expense in expenses.list:
+    spending_categories.append(expense.category)
+
+# This counter the values, and groups them in a dictionary having the keys with the most ocurrencies as values
+spending_counter = collections.Counter(spending_categories)
+top5 = spending_counter.most_common(5)
+
+# Separate keys and values into 2 lists
+category, count = zip(*top5)
+
+print(f"All ocurrencies {spending_counter}")
+print(f"Most common: {spending_counter.most_common(5)}")
+print(category)
+print(count)
+
+# Using matplotlib
+
+fig, ax = plt.subplots()
+ax.bar(category, count)
+ax.set_title('# of Purchases by Category')
+plt.show()
